23cc05b68ebd5738b2aaf838a8d16dd0f07eb580
Gabriela <101780994+emptyGabriela@users.noreply.github.com>
2023-06-12 21:09:25 -0600

    Agrego Conexion a la BDD

---
+++ baloncesto/src/app/Conexion.java
@@ -0,0 +1,15 @@
+
+package app;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Conexion {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    
+}
